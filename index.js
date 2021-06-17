652164b394e7d9e2a0e1a8bd5e952ed0840643f4
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 17:19:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase)
     return ("I can't hear you!")
+  if (string.toUpperCase)
+    return ("YES INDEED!")
   }
